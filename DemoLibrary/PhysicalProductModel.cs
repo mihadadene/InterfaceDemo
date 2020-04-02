fb4797405547fc9d9c8d5f8fd5ee6d05ac619e24
mihadadene <mi.ihadadene@gmail.com>
2020-04-01 21:30:11 -0400

    first commit

---
+++ DemoLibrary/PhysicalProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class PhysicalProductModel : PhysicalProductBase, IProductModel
+    {
+        public bool HasOrderBeenCompleted { get; private set; }
+        public string Title { get; set; }
+
+        public void ShipItem(CustomerModel customer)
+        {
+            Console.WriteLine($"Simulating shipping { Title } to {  customer.FirstName } in { customer.City }");
+            HasOrderBeenCompleted = true;
+        }
+    }
+}
